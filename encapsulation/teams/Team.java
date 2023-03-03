6292e5b42a80bffe28f8530bcf1aa64450cc6112
doksanpz <114029449+doksanpz@users.noreply.github.com>
2023-03-03 19:59:15 +0200

    Add tasks for encapsulation

---
+++ encapsulation/teams/Team.java
@@ -0,0 +1,43 @@
+package encapsulation.teams;
+
+import encapsulation.sortByNameAndAge.Person;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    Team(String name) {
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+        setName(name);
+    }
+
+    private void setName(String name) {
+        if (name.length() < 5) {
+            throw new IllegalArgumentException("Team name must be at least 5 symbols");
+        }
+        this.name = name;
+    }
+
+    void addPlayer(Person player) {
+        if (player.getAge() < 40) {
+            this.firstTeam.add(player);
+        } else {
+            this.reserveTeam.add(player);
+        }
+    }
+
+    List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+
+    List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+}
